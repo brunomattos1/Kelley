19d90332b24c36f04a0814fc042caa1ecb6643e9
Bruno Mattos <bruno_mattos@id.uff.br>
2022-09-14 15:37:12 -0300

    creating tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,14 @@
-@info "testando teste"
+using Kelley
+function g1(x::Vector{Float64})
+    z=x[1]^2+(x[2]^2)/4-1 # PRIMEIRA RESTRIÇÃO
+    return z
+end
+
+function g2(x::Vector{Float64})
+    z=(x[1]^2)/4+x[2]^2-1 # SEGUNDA RESTRIÇÃO
+    return z
+end
+
+@testset "Basic tests" begin
+    
+end
