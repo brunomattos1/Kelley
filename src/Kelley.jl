cb4c2bdfeed667449a9968cc8e88e31d3bfb8970
Bruno Mattos <bruno_mattos@id.uff.br>
2022-11-09 23:23:32 -0300

    New features

--- src/Kelley.jl
+++ src/Kelley.jl
@@ -10,36 +10,59 @@ using ForwardDiff
 export optimize!, KelleyAlgorithm
 
 function cut(
-    gi_x̄::Float64, ∇gi_x̄::Vector{Float64}, x̄::Vector{Float64}, x::Vector{VariableRef}
-) 
-    return gi_x̄ + ∇gi_x̄⋅(x - x̄) # para criar x como variavel, devemos definir em @variables
+    gi_x̄::Float64, ∇gi_x̄::Vector{Float64}, x̄::Vector{Float64}, x::Vector{VariableRef}) 
+    return gi_x̄ + ∇gi_x̄⋅(x - x̄)
 end
 
 function JuMP.optimize!(
-    ::KelleyAlgorithm, f::Function, g::Function, n::Int;
-    Jg = x -> ForwardDiff.jacobian(g,x), lb = -1e6, ub = 1e6
-)
+    ::KelleyAlgorithm, f::Function, g::Function, n::Int;lb = fill(-1e6,n), ub = fill(1e6,n),
+        Jg = x -> ForwardDiff.jacobian(g,x))
+    
     m = length(g(zeros(Float64, n)))
-    model = Model(HiGHS.Optimizer)
+    global model=Model(HiGHS.Optimizer)
     set_silent(model)
-    @variable(model, lb <= x[1:n] <= ub)
+    @variable(model, lb[i] <= x[i in 1:n] <= ub[i])
     @objective(model, Min, f(x))
     stop = false
     while !stop
         optimize!(model)
-        x̄ = JuMP.value.(x)
+        global x̄ = JuMP.value.(x)
         g_x̄ = g(x̄)
-        Jg_x̄ = Jg(x̄)
+        Jg_x̄ = Jg(x̄) 
         stop = true
+        
+        # cut deletion
+        if num_constraints(model, AffExpr, MOI.LessThan{Float64})>=10000
+            println("Over 10000 constraints")
+            cons=all_constraints(model, GenericAffExpr{Float64, VariableRef}, MOI.LessThan{Float64})
+            eval_constraints=value.(cons)
+            indice_smallest=sortperm(eval_constraints)[1:3000]
+            delete.(model, cons[indice_smallest])
+        end
+        
+        # cut add
         for i in 1:m
-            if g_x̄[i] >= 1e-6
-                @constraint(model,cut(g_x̄[i], Jg_x̄[i,:], x̄, x) <= 0)
+            if g_x̄[i]>=1e-6
+                @constraint(model,cut(g_x̄[i], Jg_x̄[i,:], x̄,x) in MOI.LessThan(0.0))
                 stop = false
             end
         end
     end
-    println("Objective value: ",objective_value(model))
-    print("Solution: ", value.(x))
+    global obj=objective_value(model)
+    global model_=model
+    print("Done!")
+end
+
+function show_objective()
+    return obj
+end
+
+function show_solution()
+    return x̄
+end
+
+function show_model()
+    return model_
 end
 
 end
